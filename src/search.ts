1b757caeb053bfd764d440bc3372612fee2ece0f
Uzgur <uzbmv2@gmail.com>
2023-09-25 20:26:51 +0500

    inital codes

---
+++ src/search.ts
@@ -0,0 +1,21 @@
+import './index';
+import { tbody } from './index';
+import './tabledata';
+
+const searchInput = document.querySelector('#searchInput') as HTMLInputElement;
+searchInput.addEventListener('input', (e: any) => {
+  const searchValue = e.target.value;
+  console.log(searchValue);
+  doSearch(searchValue);
+});
+
+function doSearch(searchValue: string) {
+  console.log(searchValue);
+  Array.from(tbody.children).forEach((rowElement: HTMLTableRowElement, index: number) => {
+    if (!rowElement.id.toLowerCase().includes(searchValue.toLowerCase())) {
+      rowElement.style.display = 'none';
+    } else {
+      rowElement.style.display = '';
+    }
+  });
+}
